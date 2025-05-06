7316fa3210143f31e4a82a4e04958950981fb282
njk02 <156973165+njk02@users.noreply.github.com>
2025-05-06 14:37:55 -0500

    Add files via upload

---
+++ AttendanceSystem/Data/Models/ClassInfo.cs
@@ -0,0 +1,15 @@
+﻿namespace AttendanceSystem.Data.Models
+{
+    public class ClassInfo
+    {
+        public int ClassID { get; set; }
+        public string ClassName { get; set; }
+    }
+
+    public class ClassStudent
+    {
+        public int ClassID { get; set; }
+        public long StudentID { get; set; }
+    }
+
+}
---
+++ AttendanceSystem/Data/Models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace AttendanceSystem.Data.Models
+{
+    public class Student
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Username { get; set; }
+        public long StudentID { get; set; }
+    }
+}
